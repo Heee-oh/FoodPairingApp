2cdc80a9bde02b5683a9df7011be126d0aeb8625
Heee-oh <siai3647@gmail.com>
2024-05-12 22:53:35 +0900

    first

--- src/main/java/FoodPair/foodpair/respository/CommentRepository.java
+++ src/main/java/FoodPair/foodpair/respository/CommentRepository.java
@@ -4,5 +4,4 @@ import FoodPair.foodpair.domain.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
-    void countAllBy();
 }
