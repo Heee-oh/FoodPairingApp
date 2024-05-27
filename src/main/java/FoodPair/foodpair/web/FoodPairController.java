3b55b947064ea9f8e63d69fa15334fc4ea19f14f
chlmins <150573382+chlmins@users.noreply.github.com>
2024-05-27 17:08:31 +0900

    Add files via upload

--- src/main/java/FoodPair/foodpair/web/FoodPairController.java
+++ src/main/java/FoodPair/foodpair/web/FoodPairController.java
@@ -1,23 +1,37 @@
 package FoodPair.foodpair.web;
 
 import FoodPair.foodpair.domain.Wine;
+import FoodPair.foodpair.respository.QuerydslFoodPairRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @Slf4j
 @Controller
 @RequestMapping("")
 public class FoodPairController {
 
+    private final QuerydslFoodPairRepository foodPairRepository;
+
+    @Autowired
+    public FoodPairController(QuerydslFoodPairRepository foodPairRepository) {
+        this.foodPairRepository = foodPairRepository;
+    }
 
     // 음식 종류와 음식 사진의 경로를 알아야됌
     // 아니면 프론트에서 따로 사진 저장 후 페어링 모델에 보내고,
     // 여기서는 음식 종류를 받고 wine 객체를 반환
 
-    @PostMapping("/wine/{foodName}")
-    public Wine pairing(@PathVariable String foodName) {
-
-        return null;
+    @GetMapping("/wine/{foodName}")
+    @ResponseBody
+    public List<Wine> pairing(@PathVariable String foodName) {
+        List<Wine> pairedWine = foodPairRepository.findPairingWine(foodName);
+        if (pairedWine.isEmpty()) {
+            return foodPairRepository.findDefaultWines();
+        }
+        return pairedWine;
     }
+
 }
